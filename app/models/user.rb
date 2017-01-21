1e98796898e574f16e657190df764fd5432f3d51
Chad Lucas <chad.lucas.19@gmail.com>
2017-01-21 02:34:39 -0600

    users can sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-	validates :name, presence: true, uniqueness: true
-	validates :email, presence: true, uniqueness: true
+	validates :name, presence: true
+	validates :email, presence: true
 
 	has_secure_password
-	validates :password, presence: true, length: { minimum: 6}
+	validates :password, presence: true, length: { minimum: 3}
 end
